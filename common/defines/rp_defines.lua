a18739c46cdf43bba67c630c1b6d6de4598de626
mariupolbase <awekweka090mail.com@gmail.com>
2024-07-02 18:56:52 +0500

    новини
    дауны

---
+++ common/defines/rp_defines.lua
@@ -0,0 +1,9 @@
+NDefines.NGame.START_DATE = "1993.1.1.1"
+NDefines.NGame.END_DATE = "2000.1.1.1"
+NDefines.NCountry.MIN_STABILITY = -1.0
+NDefines.NCountry.MIN_WAR_SUPPORT = -1.0
+NDefines.NBuildings.BASE_FACTORY_REPAIR = 0.2
+NDefines.NBuildings.BASE_FACTORY_REPAIR_FACTOR = 0.95
+NDefines.NMilitary.CORPS_COMMANDER_DIVISIONS_CAP = 10
+NDefines.NMilitary.FIELD_MARSHAL_DIVISIONS_CAP = 10
+NDefines.NMilitary.REINFORCE_CHANCE = 0.04
